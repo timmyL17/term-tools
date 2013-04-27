3237d8a9e0c1e9feac7adc8adb5fa7bd5b131a96
Tim Langlois <langlois@cs.cornell.edu>
2013-04-27 12:26:45 -0400

    fixed autojump installer

--- installers/autojump.sh
+++ installers/autojump.sh
@@ -2,13 +2,14 @@
 set -e
 
 # install autojump for both bash and zsh (if installed)
+echo "Installing autojump into $HOME ..."
+cd autojump
+./install.sh > /dev/null
+cd ..
+
 TMP_AUTOJUMP_SH=~/.autojump-term-tools.tmp
 for f in zsh bash; do
-	echo "Installing autojump into $HOME ($f)..."
 	SRC_AUTOJUMP_SH=~/.autojump/etc/profile.d/autojump.$f
-	cd autojump
-	./install.sh --$f > /dev/null
-	cd ..
 	sed 's/^function j {$/function j_impl {/' < $SRC_AUTOJUMP_SH > $TMP_AUTOJUMP_SH
 	mv -f $TMP_AUTOJUMP_SH $SRC_AUTOJUMP_SH
 done
