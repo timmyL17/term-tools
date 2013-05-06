b9c2465b4301e2b08c8a12c593a3936aecf74551
Tim Langlois <langlois@cs.cornell.edu>
2013-05-05 21:46:19 -0400

    updated powerline installer

--- installers/powerline.sh
+++ installers/powerline.sh
@@ -4,5 +4,9 @@ set -e
 mkdir -p ~/.fonts
 cp ~/term-tools/vim/bundle/powerline/font/PowerlineSymbols.otf ~/.fonts
 sudo fc-cache -vf ~/.fonts
+
 mkdir -p ~/.config/fontconfig/conf.d
 cp ~/term-tools/vim/bundle/powerline/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+mkdir -p ~/.fonts.conf.d
+cp ~/term-tools/vim/bundle/powerline/font/10-powerline-symbols.conf ~/.fonts.conf.d/
